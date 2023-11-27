9a9eda400d183752ef7ee7782f4d711812862b22
unknown <rezamsanitrrga@gmail.com>
2023-11-27 21:07:36 +0330

    Django-store

---
+++ home_module/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+from django.views.generic.base import TemplateView
+
+from site_module.models import SiteSetting, FooterLinkBox, Slider
+
+
+class HomeView(TemplateView):
+    template_name = 'home_module/index_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        sliders = Slider.objects.filter(is_active=True)
+        context['sliders'] = sliders
+        return context
+
+
+def site_header_component(request):
+    setting: SiteSetting = SiteSetting.objects.filter(is_main_setting=True).first()
+    context = {
+        'site_setting': setting
+    }
+    return render(request, 'shared/site_header_component.html', context)
+
+
+def site_footer_component(request):
+    setting: SiteSetting = SiteSetting.objects.filter(is_main_setting=True).first()
+    footer_link_boxes = FooterLinkBox.objects.all()
+    context = {
+        'site_setting': setting,
+        'footer_link_boxes': footer_link_boxes
+    }
+    return render(request, 'shared/site_footer_component.html', context)
+
+
+class AboutView(TemplateView):
+    template_name = 'home_module/about_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+        site_setting: SiteSetting = SiteSetting.objects.filter(is_main_setting=True).first()
+        context['site_setting'] = site_setting
+        return context
---
+++ product_module/views.py
@@ -0,0 +1,83 @@
+from django.db.models import Count
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import View
+from .models import Product, ProductCategory, ProductBrand
+
+
+class ProductListView(ListView):
+    template_name = 'product_module/product_list.html'
+    model = Product
+    context_object_name = 'products'
+    ordering = ['-price']
+    paginate_by = 1
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        print('context_data')
+        context = super(ProductListView, self).get_context_data()
+        query = Product.objects.all()
+        product: Product = query.order_by('-price').first()
+        db_max_price = product.price if product is not None else 0
+        context['db_max_price'] = db_max_price
+        context['start_price'] = self.request.GET.get('start_price') or 0
+        context['end_price'] = self.request.GET.get('end_price') or db_max_price
+        return context
+
+    def get_queryset(self):
+        print('query_set')
+        query = super(ProductListView, self).get_queryset()
+        category_name = self.kwargs.get('cat')
+        brand_name = self.kwargs.get('brand')
+        request: HttpRequest = self.request
+        start_price = request.GET.get('start_price')
+        end_price = request.GET.get('end_price')
+        if start_price is not None:
+            query = query.filter(price__gte=start_price)
+
+        if end_price is not None:
+            query = query.filter(price__lte=end_price)
+
+        if brand_name is not None:
+            query = query.filter(brand__url_title__iexact=brand_name)
+
+        if category_name is not None:
+            query = query.filter(category__url_title__iexact=category_name)
+        return query
+
+
+class ProductDetailView(DetailView):
+    template_name = 'product_module/product_detail.html'
+    model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_product = self.object
+        request = self.request
+        favorite_product_id = request.session.get("product_favorites")
+        context['is_favorite'] = favorite_product_id == str(loaded_product.id)
+        return context
+
+
+class AddProductFavorite(View):
+    def post(self, request):
+        product_id = request.POST["product_id"]
+        product = Product.objects.get(pk=product_id)
+        request.session["product_favorites"] = product_id
+        return redirect(product.get_absolute_url())
+
+
+def product_categories_component(request: HttpRequest):
+    product_categories = ProductCategory.objects.filter(is_active=True, is_delete=False)
+    context = {
+        'categories': product_categories
+    }
+    return render(request, 'product_module/components/product_categories_component.html', context)
+
+
+def product_brands_component(request: HttpRequest):
+    product_brands = ProductBrand.objects.annotate(products_count=Count('product')).filter(is_active=True)
+    context = {
+        'brands': product_brands
+    }
+    return render(request, 'product_module/components/product_brands_component.html', context)
